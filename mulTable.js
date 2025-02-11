91dab889d877da81496b5970ed90fbd58ee5047c
samin710 <smsaminii112@gmail.com>
2025-02-11 20:42:44 +0600

    while mulTable done

--- mulTable.js
+++ mulTable.js
@@ -1,5 +1,12 @@
 // Generate a multiplication table for number 9
 
+console.log("Using for loop:");
 for(let i = 1; i<=10; i++){
     console.log("9 x",i,"=",9*i);
+}
+console.log("Using while loop:");
+let i = 1;
+while (i<=10) {
+    console.log("5 x", i, "=", 5 * i);
+    i++;
 }
