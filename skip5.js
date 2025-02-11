b0c6e50287dbb7dcfe801d0d8e30dab96c3bfdcb
samin710 <smsaminii112@gmail.com>
2025-02-11 21:05:27 +0600

    skip5 done

---
+++ skip5.js
@@ -0,0 +1,8 @@
+// display odd number from 55 to 85 and skip the numbers divisible by 5.
+
+for (let i = 55; i <= 85; i++) {
+  if (i % 5 === 0 || i % 2 === 0) {
+    continue;
+  }
+  console.log(i);
+}
