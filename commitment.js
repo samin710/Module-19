4cae7fbb78d8919dd18f95ecc47baf5e962a996b
samin710 <smsaminii112@gmail.com>
2025-02-11 20:56:48 +0600

    while commitment done

--- commitment.js
+++ commitment.js
@@ -1,8 +1,19 @@
 // "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
 
+console.log("Using for loop:");
 for (let i = 1; i <= 60; i++) {
   console.log(i);
   console.log(
     "I will invest at least 6 hrs every single day for next 60 days!"
   );
+}
+
+console.log("Using while loop:");
+let i = 1;
+while (i <= 60) {
+  console.log(i);
+  console.log(
+    "I will invest at least 6 hrs every single day for next 60 days!"
+  );
+  i++;
 }
