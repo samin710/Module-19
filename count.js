f05ab63f433d1cf9a86b5c40e8e47ebac334d01c
samin710 <smsaminii112@gmail.com>
2025-02-11 20:40:30 +0600

    while count done

--- count.js
+++ count.js
@@ -1,5 +1,13 @@
 // Implement a countdown timer that counts down from 81 to 65.
 
+console.log("Using for loop:");
 for(let i = 81; i>=65; i--){
     console.log(i);
+}
+
+console.log("Using while loop:");
+let i = 21;
+while (i>=15) {
+    console.log(i);
+    i--;
 }
