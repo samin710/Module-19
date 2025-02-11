36ec78670a2a9c0a02f329554609f67b1761c0cf
samin710 <smsaminii112@gmail.com>
2025-02-11 20:54:36 +0600

    while oddEven done

--- oddEven.js
+++ oddEven.js
@@ -6,18 +6,36 @@
 
 // Find all the even numbers from 78 to 98.
 
+console.log("Using for loop:");
 console.log("Task - 1");
 
 for (let i = 61; i <= 100; i++) {
-    if(i % 2 !== 0){
-        console.log(i);
-    }
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
 }
 console.log("Task - 2");
 
 for (let i = 78; i <= 98; i++) {
-    if(i % 2 === 0){
-        console.log(i);
-    }
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+console.log("Using while loop:");
+console.log("Task - 1");
+let i = 61;
+while (i <= 100) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+  i++;
 }
 
+console.log("Task - 2");
+i = 78;
+while (i <= 98) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+  i++;
+}
